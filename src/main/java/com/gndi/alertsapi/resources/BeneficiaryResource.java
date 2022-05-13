3544cc4e91ded38c7db925132a8b34ff6fbf7a2d
felipevasconcelos1 <felipeevss@gmail.com>
2022-05-13 10:58:22 -0300

    service layer, component registration

--- src/main/java/com/gndi/alertsapi/resources/BeneficiaryResource.java
+++ src/main/java/com/gndi/alertsapi/resources/BeneficiaryResource.java
@@ -1,13 +1,17 @@
 package com.gndi.alertsapi.resources;
 
 import com.gndi.alertsapi.entities.Beneficiary;
+import com.gndi.alertsapi.services.BeneficiaryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/beneficiaries")
@@ -15,9 +19,18 @@ public class BeneficiaryResource {
 
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
+    @Autowired
+    private BeneficiaryService service;
+
     @GetMapping
-    public ResponseEntity<Beneficiary> findAll() throws ParseException {
-        Beneficiary beneficiary = new Beneficiary(39886097, 1726874, "70017", 592.01, sdf.parse("2022/05/11 07:18:28"), 0, 0, 0, null, null, null);
-        return ResponseEntity.ok().body(beneficiary);
+    public ResponseEntity<List<Beneficiary>> findAll() throws ParseException {
+        List<Beneficiary> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Beneficiary> findById(@PathVariable Integer id) {
+        Beneficiary object = service.findbyId(id);
+        return ResponseEntity.ok().body(object);
     }
 }
---
+++ src/main/java/com/gndi/alertsapi/services/BeneficiaryService.java
@@ -0,0 +1,26 @@
+package com.gndi.alertsapi.services;
+
+import com.gndi.alertsapi.entities.Beneficiary;
+import com.gndi.alertsapi.repositories.BeneficiaryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BeneficiaryService {
+
+    @Autowired
+    private BeneficiaryRepository repository;
+
+    public List<Beneficiary> findAll() {
+        return repository.findAll();
+    }
+
+    public Beneficiary findbyId(Integer id) {
+        Optional<Beneficiary> object = repository.findById(id);
+        return object.get();
+    }
+}
