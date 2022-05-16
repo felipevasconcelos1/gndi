d89fbb8bd9e9112882aa37be9c68cade70a835d2
felipevasconcelos1 <felipeevss@gmail.com>
2022-05-16 14:16:19 -0300

    spaces between atributes

--- src/main/java/com/gndi/alertsapi/entities/Alert.java
+++ src/main/java/com/gndi/alertsapi/entities/Alert.java
@@ -27,7 +27,6 @@ public class Alert implements Serializable {
     private Integer nonPsychiatricHospitalizationDays;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant lastDischargeDate;
-
     private Double totalContractCost;
     private Double totalCoparticipationCosts;
     private Double totalContractRevenue;
